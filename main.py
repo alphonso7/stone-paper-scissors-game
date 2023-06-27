e1c62ec43dc77512949cd645b9e0da524b8fe979
Jayesh Nehare <137815704+alphonso7@users.noreply.github.com>
2023-06-27 08:57:06 +0530

    Update main.py
    Delete unnecessary lines of code

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-'''
-
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
 import random
 print("Let's play Stone paper scissors game")
 print("---------------------")
